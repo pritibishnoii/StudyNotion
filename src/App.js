7c5bdba128a88fc054e53a9cb9dd057aa828089a
priti <priti12bishnoi@gmail.com>
2024-08-09 21:37:00 +0530

    add homePage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from './pages/Home'
+
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-xl  text-black-400">React App</h1>
+    <div className="w-screen min-h-screen bg-richblack-900 flex flex-col   font-inter">
+      <Routes>
+        <Route path="/" element={ <Home/>} />
+      </Routes>
     </div>
   );
 }
